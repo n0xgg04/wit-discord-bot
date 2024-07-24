e950963bb45cb13bebc4e17edec048138b932b24
n0xgg04 <dulieu.vblc@gmail.com>
2024-07-24 20:10:03 +0700

    AA

--- src/utils/dateParse.ts
+++ src/utils/dateParse.ts
@@ -1,6 +1,9 @@
+import moment from "moment-timezone";
+
+moment.tz("Asia/Ho_Chi_Minh");
 export default function dateParse(data: Date | string): Date {
     if (typeof data === "string") {
-        return new Date(data);
+        return moment(data).toDate();
     }
     return data;
 }
